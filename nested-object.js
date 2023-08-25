557510ad209ad0e7efb164558f1920698931b625
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2023-08-25 11:07:58 +0600

    map part

---
+++ nested-object.js
@@ -0,0 +1,11 @@
+
+
+
+// const cube=x=> x*x*x; 
+// console.log(cube(2))
+// const {x, y, z} = {x: 1, y1: 2, z: 3};
+// console.log(y);
+
+const nums = [1,2,3,4,5];
+let output = nums.filter(n => n%2);
+console.log(output);
