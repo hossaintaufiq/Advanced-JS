1a246f0e494bc2f7b9530c80bc24047868bd3083
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2023-08-28 08:53:44 +0600

    hello

---
+++ map.js
@@ -0,0 +1,15 @@
+//  map 
+
+// types of map using filter function 
+
+const number = [1,2,2,566,9,8,5,6,4,,5];
+
+// const selected = number.filter(n=> n>10);
+const oddNumbers = number.filter(n=> n%2!=0);
+// console.log(selected);
+console.log(oddNumbers)
+
+
+
+// have to look 
+//  class , map , filter , find , reduce , array slice , 
---
+++ practice.js
@@ -0,0 +1,11 @@
+// have to look 
+//  class , map , filter , find , reduce , array slice  concepts and practices  
+// video 30-2 for interview 
+
+
+//  some practices 
+// == and triple === 
+
+// == checks the values and === checks the values with the data types 
+// always use === for play safe 
+// double == does type coercion (change the type to the same type )
